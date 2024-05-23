ad65d935605b6b1f8c216e82e0e992288cd1c65d
itsjohncs <johnsullivan.pem@gmail.com>
2024-05-23 16:56:00 -0700

    Support `npm link`-ing `shmeppy-public`.
    It looks like the module resolution was using the real path of the
    package to find the nearest `node_modules` so it couldn't find the
    globally installed packages.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: [
         "@shmeppy/dumb-dice-components",
         "@shmeppy/client-utils",
     ],
+    webpack(webpackConfig) {
+        webpackConfig.resolve.alias["@shmeppy/dumb-dice-components"] =
+            path.resolve(
+                import.meta.dirname,
+                "./node_modules/@shmeppy/dumb-dice-components",
+            );
+        webpackConfig.resolve.alias["@shmeppy/client-utils"] = path.resolve(
+            import.meta.dirname,
+            "./node_modules/@shmeppy/client-utils",
+        );
+
+        return {
+            ...webpackConfig,
+        };
+    },
 };
 
 export default nextConfig;
